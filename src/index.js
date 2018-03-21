62cc412fd7a97ed2b5e9e1b489f8c18597a7f999
Brendan Walker <brenny_walker@hotmail.com>
2018-03-21 09:37:43 -0700

    Set up most dependancies, configured routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+
+import './styles/css/index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
